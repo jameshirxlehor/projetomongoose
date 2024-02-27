6e92ca83596d06b6a4125ff1f9c4a2ba156915c8
James Hirxlehor <jameshirx@gmail.com>
2024-02-27 13:11:09 -0300

    Iniciando o projeto e implmentando o metodo salvar

---
+++ controllers/ProductController.js
@@ -0,0 +1,14 @@
+const Product = require('../models/Product');
+
+module.exports = class ProductController{
+    static showProducts(req,res){
+        res.render("products/all");
+    }
+
+
+
+
+
+
+
+};
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express'); // habilita a utilização do express para criação de api
+const exphbs = require('express-handlebars'); // habilita a utilização de handlebars para uttilizar uma layout padrão
+
+
+const app = express(); //estancia o objeto express para criação de rotas e midlewares
+
+
+const conn = require('./db/conn'); //captura o modulo para acessar o banco de dados
+
+
+const productRoutes = require('./routes/productsRoutes'); //importando modulo para acessar as rotas (GET,POST,PUT,...)
+
+
+app.engine('handlebars',exphbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views','./views');
+
+
+//responsábel por fazer a leitura do body
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+
+
+app.use(express.json());//habiliza o uso de JSON
+
+
+app.use(express.static('public'));
+
+
+app.use('/products', productRoutes);
+
+
+app.listen(3000); //habilita a porta que iremos utlizar no localhost
+
+
